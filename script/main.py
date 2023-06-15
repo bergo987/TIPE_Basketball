06a0a157b9af11f66ecf050bc97c41b8c2041a05
bergo987 <hu.berthod11@gmail.com>
2023-06-15 21:12:53 +0200

    On compte officiellement les points

--- script/main.py
+++ script/main.py
@@ -30,11 +30,21 @@ mid_h = 0
 bu_pos_1 = np.array([]) # initialisation des tableaux pour la position
 bu_pos_2 = np.array([])
 
+count_temp = 0 
+
+def scored(x,y,x2,y2,x3,y3): 
+    if x <x3 and x3 <x2 and y< y2 and y2< y3 :
+        return True
+    else :
+        return False 
+
 
 
 while True:
     # Capture frame from the video
     isclosed = 0
+    point_count = 0 
+
     # Initialisation du flux vidéo, pour pouvoir le réutiliser facilement 
     cap = cv2.VideoCapture('/Users/hugo/Documents/Cours/Prepa/TIPE/TIPE_Baskettball/script/IA_assistef/01.mp4')
     #Récuperation de la hauteur, de la largeur et de leurs moitiés 
@@ -74,12 +84,10 @@ while True:
         # Création du masque pour isolé la balle et les paniers
         ball_mask = cv2.inRange(hsv, lower_ball, upper_ball)
 
-
         # Apply morphological transformations to the mask
         opening_ball = cv2.morphologyEx(ball_mask, cv2.MORPH_OPEN, kernel)
         closing_ball = cv2.morphologyEx(opening_ball, cv2.MORPH_CLOSE, kernel)
 
-        
         # Find contours of the basketball
         contours_ball, hierarchy_ball = cv2.findContours(closing_ball, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours_bu , hierarchy_bu = cv2.findContours(closing_bu,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -100,7 +108,7 @@ while True:
                 cv2.rectangle(frame, (xb,yb),(xb+wb,yb+hb), (255,0,0),2)
                 y_area = yb + 50
                 x_area = xb 
-                y_area_2 = y_area +15
+                y_area_2 = y_area +2
                 x_area_2 = xb + wb
                 cv2.rectangle(frame, (x_area,y_area),(x_area_2,y_area_2),(0,0,255), 2 )
                 bu_count += 1
@@ -119,7 +127,12 @@ while True:
                 x, y, w, h = cv2.boundingRect(element)
                 cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
                 ball_count += 1
-        
+                if scored(x_area,y_area,x_area_2,y_area_2,x,y) and count_temp > 10 : 
+                    point_count += 2
+                    count_temp = 0 
+        count_temp+=1
+
+        print("nb de points marqué : ", point_count)
         if ball_count > 1 :
             print("on a un problème quelque part, on detecte 2 balles")
 
@@ -128,10 +141,12 @@ while True:
         cv2.putText(frame, "nb panier: " + str(bu_count), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
 
         cv2.putText(frame, "taille: "+str(width)+" x "+str(height), (10, 90), cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        cv2.putText(frame, "points: "+str(point_count) + " count_temp"+str(count_temp), (10, 120), cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
         
         cv2.imshow("Ball Count", ball_mask)
         cv2.imshow("Bu Count", bu_mask)
         cv2.imshow("Basketball Tracker", frame)
+        
         #cv2.imshow("sans rien",vide)
         # Exit the program if the 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
