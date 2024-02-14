54269d5267c78ba59678a23586821c6cd248d110
bergo987 <hu.berthod11@gmail.com>
2024-02-14 19:27:04 +0100

    on recomme tout le code à zero afin de commencer par détecter les paniers

--- script/Kalman.py
+++ script/Kalman.py
@@ -1,9 +1,10 @@
 import numpy as np 
+import cv2
 
 class KalmanFilter(object): 
     """dt : temps d'actualisation 
-    point : coordonné du point
-    Erreur : plus l'entier est grand, plus le filtre pense que c'est brouillé """
+    point : coordonné initiaux du point
+    erreur : plus l'entier est grand, plus le filtre pense que c'est brouillé """
     def __init__ (self, dt, point, erreur):
         self.dt = dt
         self.erreur = int(erreur)
@@ -48,3 +49,53 @@ class KalmanFilter(object):
         self.P=(I-(K*self.H))*self.P
 
         return self.E
+class Annexe(object):
+    def __init__(self,w,h,mw,mh):
+        self.width = w
+        self.height = h
+        self.mid_w = mw
+        self.mid_h = mh       
+
+    def scored(self, x,y,x2,y2,x3,y3): 
+        if x <x3 and x3 <x2 and y< y2 and y2< y3 :
+            return True
+        else :
+            return False 
+        
+
+    def detect_ball(self, image, min_surface,max_surface,lo,hi):
+        """Renvoie un tableau trié par surface décroissante"""
+        points=np.empty(2,int)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image=cv2.blur(image, (10, 10))
+        mask=cv2.inRange(image, lo, hi)
+        mask=cv2.erode(mask, None, iterations=4)
+        mask=cv2.dilate(mask, None, iterations=4)
+        elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        elements=sorted(elements, key=lambda x:cv2.contourArea(x), reverse=True)
+        for element in elements:
+            if cv2.contourArea(element)>min_surface and cv2.contourArea(element)<max_surface:
+                ((x, y), _)=cv2.minEnclosingCircle(element)
+                points =np.append(points,np.array([int(x), int(y)]))
+            else:
+                break
+        return points,mask 
+    
+
+    def detect_bu(self, frame,min_surface,lo,hi): 
+        """Renvoie un tableau trié par surface décroissante"""
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        image=cv2.blur(hsv, (10, 10))
+        bu_mask=cv2.inRange(image, lo, hi)
+        bu_mask=cv2.erode(bu_mask, None, iterations=4)
+        bu_mask=cv2.dilate(bu_mask, None, iterations=4)
+        contours_bu=cv2.findContours(bu_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        contours_bu=sorted(contours_bu, key=lambda x:cv2.contourArea(x), reverse=True)
+        bu_count = 0 
+        for countour in contours_bu : 
+            area_bu = cv2.contourArea(countour)
+            xb,yb,wb,hb = cv2.boundingRect(countour)
+            if area_bu > min_surface and  yb < self.height and 2*self.mid_w<xb < self.width :
+                cv2.rectangle(image, (xb,yb),(xb+wb,yb+hb), (255,255,255),2)
+                bu_count += 1
+        return bu_count, bu_mask
--- script/test.py
+++ script/test.py
@@ -1,7 +1,19 @@
-for t in range(tmax):
-    if t % T/2 : 
-        print("gauche \n")
-    elif t % T :
-        print("droite\n")
-    else: 
-        print("ni gauche ni droite")
+import numpy as np
+
+x = 4 
+y = 5 
+
+u = 6
+v = 9 
+tab = []
+
+
+tab = np.append([],(int(x),int(y)))
+print(tab) 
+
+tab = np.append(tab,(int(u),int(v)))
+print(tab)
+
+
+for i in range(2): 
+    print(tab[i][0],tab[i][1])
